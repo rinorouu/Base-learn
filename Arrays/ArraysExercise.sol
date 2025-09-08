8f21c9c501edaba83f1badfb746029fe15191808
rinorouu <rinorouu@gmail.com>
2025-09-08 14:30:38 +0700

    Update ArraysExercise.sol

--- Arrays/ArraysExercise.sol
+++ Arrays/ArraysExercise.sol
@@ -1,1 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract ArraysExercise {
+    uint[] public numbers = [1,2,3,4,5,6,7,8,9,10];
+
+    address[] public senders;
+    uint[] public timestamps;
+
+    function getNumbers() external view returns (uint[] memory) {
+        return numbers;
+    }
+
+    function resetNumbers() public {
+        // lebih gas-efisien tanpa .push()
+        numbers = [1,2,3,4,5,6,7,8,9,10];
+    }
+
+    function appendToNumbers(uint[] calldata _toAppend) public {
+        for (uint i = 0; i < _toAppend.length; i++) {
+            numbers.push(_toAppend[i]);
+        }
+    }
+
+    function saveTimestamp(uint _unixTimestamp) public {
+        senders.push(msg.sender);
+        timestamps.push(_unixTimestamp);
+    }
+
+    function afterY2K() public view returns (uint[] memory, address[] memory) {
+        uint count = 0;
+
+        for (uint i = 0; i < timestamps.length; i++) {
+            if (timestamps[i] > 946702800) {
+                count++;
+            }
+        }
+
+        uint[] memory filteredTimestamps = new uint[](count);
+        address[] memory filteredSenders = new address[](count);
+
+        uint idx = 0;
+        for (uint i = 0; i < timestamps.length; i++) {
+            if (timestamps[i] > 946702800) {
+                filteredTimestamps[idx] = timestamps[i];
+                filteredSenders[idx] = senders[i];
+                idx++;
+            }
+        }
+
+        return (filteredTimestamps, filteredSenders);
+    }
+
+    function resetSenders() public {
+        delete senders;
+    }
+
+    function resetTimestamps() public {
+        delete timestamps;
+    }
+}
